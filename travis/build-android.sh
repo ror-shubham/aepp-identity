4097da4c6d04a83e3c043619ac2e717d7ea085b1
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-06-19 09:24:16 +0200

    Temp

--- travis/build-android.sh
+++ travis/build-android.sh
@@ -2,7 +2,12 @@
 
 set -e
 
+npm run build
+
+cordova platform rm android
+cordova platform add android
+
 # Build Ionic App for Android
-cordova platform add android --nofetch
+# cordova platform add android --nofetch
 
 cordova build android
